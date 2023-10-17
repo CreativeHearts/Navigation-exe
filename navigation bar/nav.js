76c2ec9f209220b2f2ff6e3a347a2b1a95ebaa55
omodiagbe heart joshua <132420736+CreativeHearts@users.noreply.github.com>
2023-10-17 03:19:01 -0700

    Add files via upload

---
+++ navigation bar/nav.js
@@ -0,0 +1,8 @@
+/** @format */
+"nav-open";
+const iconMain = document.querySelector(".nav-main");
+const navMain = document.querySelector(".nav-main");
+
+navMain.addEventListener("click", function () {
+  iconMain.classList.toggle("nav-open");
+});
